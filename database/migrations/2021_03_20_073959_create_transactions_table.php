a97918689939bf2eab4171585a61f4cbdd7532e3
chrysthlyn <chrysthlyn@gmail.com>
2021-09-04 23:00:50 +0700

    Add Transactions Table

--- database/migrations/2021_03_20_073959_create_transactions_table.php
+++ database/migrations/2021_03_20_073959_create_transactions_table.php
@@ -15,6 +15,7 @@ class CreateTransactionsTable extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('travel_packages_id');
             $table->integer('user_id')->nullable();
             $table->integer('additional_visa');
             $table->integer('transaction_total');
