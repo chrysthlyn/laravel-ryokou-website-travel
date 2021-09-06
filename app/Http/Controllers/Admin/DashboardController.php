cc12ce712a0ac5b7bdd38cdb87ee81c3dd5c8cb6
chrysthlyn <chrysthlyn@gmail.com>
2021-09-07 01:01:01 +0700

    Update DashboardController

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\TravelPackage;
+use App\Transaction;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        return view('pages.admin.dashboard');
+        return view('pages.admin.dashboard', [
+            'travel_package' => TravelPackage::count(),
+            'transaction' => Transaction::count(),
+            'transaction_pending' => Transaction::where('transaction_status', 'PENDING')->count(),
+            'transaction_success' => Transaction::where('transaction_status', 'SUCCESS')->count(),
+        ]);
     }
 }
