e7f605f546a23573d49d19ce00b3c3da0e14f310
chrysthlyn <chrysthlyn@gmail.com>
2021-09-07 01:02:22 +0700

    Update DetailController

--- app/Http/Controllers/DetailController.php
+++ app/Http/Controllers/DetailController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\TravelPackage;
 use Illuminate\Http\Request;
 
 class DetailController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request, $slug)
     {
-        return view('pages.detail');
+        $item = TravelPackage::with(['galleries'])
+        ->where('slug', $slug)
+        ->firstOrFail();
+        return view('pages.detail', [
+            'item' => $item
+        ]);
     }
 }
