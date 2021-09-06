35552f6fd72d67e663a93bb4b73aeb9f28f0d70e
chrysthlyn <chrysthlyn@gmail.com>
2021-09-06 20:02:58 +0700

    Update Feature Transaction

---
+++ app/Transaction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Transaction extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'travel_packages_id', 'user_id', 'additional_visa',
+        'transaction_total', 'transaction_status'
+    ];
+
+    protected $hidden = [];
+
+    public function details(){
+        return $this->hasMany(TransactionDetail::class, 'transactions_id', 'id');
+    }
+
+    public function travel_package(){
+        return $this->belongsTo(TravelPackage::class, 'travel_packages_id', 'id');
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
