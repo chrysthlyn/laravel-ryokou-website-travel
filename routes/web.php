cc012bcc857c9665268fc1358aba21485e587a8d
chrysthlyn <chrysthlyn@gmail.com>
2021-09-07 01:09:04 +0700

    Update Routing

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,29 @@
 
 Route::get('/', 'HomeController@index')
     ->name('home');
-Route::get('/detail', 'DetailController@index')
+
+Route::get('/detail/{slug}', 'DetailController@index')
     ->name('detail');
-Route::get('/checkout', 'CheckoutController@index')
-    ->name('checkout');
-Route::get('/checkout/success', 'CheckoutController@success')
-    ->name('checkout-success');
+
+Route::post('/checkout/{id}', 'CheckoutController@process')
+    ->name('checkout_process')
+    ->middleware(['auth', 'verified']);
+
+Route::get('/checkout/{id}', 'CheckoutController@index')
+    ->name('checkout')
+    ->middleware(['auth', 'verified']);
+
+Route::post('/checkout/create/{detail_id}', 'CheckoutController@create')
+    ->name('checkout-create')
+    ->middleware(['auth', 'verified']);
+
+Route::get('/checkout/remove/{detail_id}', 'CheckoutController@remove')
+    ->name('checkout-remove')
+    ->middleware(['auth', 'verified']);
+    
+Route::get('/checkout/confirm/{id}', 'CheckoutController@success')
+    ->name('checkout-success')
+    ->middleware(['auth', 'verified']);
 
 Route::prefix('admin')
     ->namespace('Admin')
