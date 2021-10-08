3383177c10551a7e76f62dbc617987b05c7ba714
chrysthlyn <chrysthlyn@gmail.com>
2021-10-08 23:42:06 +0700

    Fixed Some Error

--- database/migrations/2021_03_20_073959_create_transactions_table.php
+++ database/migrations/2021_03_20_073959_create_transactions_table.php
@@ -16,7 +16,7 @@ class CreateTransactionsTable extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->integer('travel_packages_id');
-            $table->integer('user_id')->nullable();
+            $table->integer('users_id')->nullable();
             $table->integer('additional_visa');
             $table->integer('transaction_total');
             $table->string('transaction_status');   // IN_CART, PENDING, SUCCESS, CANCEL, FAILED
